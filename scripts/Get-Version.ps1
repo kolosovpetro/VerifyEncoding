231c446917884e329511c3c114f1a1dfaad70b5c
Friedrich von Never <friedrich@fornever.me>
2025-02-08 23:43:54 +0100

    CI: use default version if not available

--- scripts/Get-Version.ps1
+++ scripts/Get-Version.ps1
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 Friedrich von Never <friedrich@fornever.me>
+# SPDX-FileCopyrightText: 2024-2025 Friedrich von Never <friedrich@fornever.me>
 #
 # SPDX-License-Identifier: MIT
 
@@ -15,15 +15,8 @@ if ($RefName -match '^refs/tags/v') {
     $version = $RefName -replace '^refs/tags/v', ''
     Write-Host "Pushed ref is a version tag, version: $version"
 } else {
-    $propsFilePath = "$RepositoryRoot/Directory.Build.props"
-    [xml] $props = Get-Content $propsFilePath
-    foreach ($group in $props.Project.PropertyGroup) {
-        if ($group.Label -eq 'Packaging') {
-            $version = $group.Version
-            break
-        }
-    }
-    Write-Host "Pushed ref is a not version tag, got version from $($propsFilePath): $version"
+    $version = 'next'
+    Write-Host "Pushed ref is a not version tag, using version $version"
 }
 
 Write-Output $version
